b71bf38588c89aebc6b4d52a177b607ebd7101fd
TaslimOwolarafe <owolarafetaslim@gmail.com>
2022-04-30 18:03:04 +0100

    styled-1 forms

--- Django_5/learning_users/basic_app/forms.py
+++ Django_5/learning_users/basic_app/forms.py
@@ -1,5 +1,6 @@
+# from xml.dom.minidom import Attr
 from django import forms
-from django.contrib.auth.models import User
+from basic_app.models import User
 from basic_app.models import UserProfileInfo
 
 class UserForm(forms.ModelForm):
@@ -7,9 +8,20 @@ class UserForm(forms.ModelForm):
 
     class Meta():
         model = User
-        fields = ('username','email','password')
+        fields =['username','email','password']
+
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "form-control", "id": "username"}),
+            "email": forms.EmailInput(attrs={"class": "form-control", "id": "email"}),
+            "password": forms.PasswordInput(attrs={"class": "form-control", "id": "password"}),
+        }
         
 class UserProfileInfoForm(forms.ModelForm):
     class Meta():
         model = UserProfileInfo
-        fields = ('portfolio_site', 'profile_pic')
+        fields = ['portfolio_site', 'profile_pic']
+
+        widgets = {
+            "portfolio_site": forms.URLInput(attrs={"class": "form-control", "id": "portfolio"}),
+            "profile_pic": forms.FileInput(attrs={"class": "form-control", "id": "picture"}),
+        }
